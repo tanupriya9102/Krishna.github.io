3d08332a861356db503618ca51e99170e7768b2d
tanupriya9102 <pathak.tanupriya@gmail.com>
2024-01-26 04:15:28 +0530

    commit

--- script.js
+++ script.js
@@ -1,76 +1,76 @@
-let highestZ=1;
 
-class Paper{
-    holdingPaper=false;
-    prevMouseX=0;
-    prevMouseY=0;
+let highestZ = 1;
 
-    mouseX=0;
-    mouseY=0;
-    velocityX=0;
-    velocityY=0;
-    currentPaperX=0;   
-    currentPaperY=0;
-    init(paper){
-        paper.addEventListener('mousedown',(e)=>{
-            this.holdingPaper = true;
-            paper.style.zIndex=highestZ;
-            highestZ+=1;
+class Paper {
+  holdingPaper = false;
+  prevMouseX = 0;
+  prevMouseY = 0;
 
-            if(e.button===0){
-                this.prevMouseX=this.mouseX;
-                this.prevMouseY=this.mouseY;
+  mouseX = 0;
+  mouseY = 0;
+  velocityX = 0;
+  velocityY = 0;
+  currentPaperX = 0;
+  currentPaperY = 0;
 
-                console.log(this.prevMouseX);
-                console.log(this.prevMouseY);
-            }
-        })
+  constructor(paper) {
+    this.paper = paper;
+    this.audio = document.getElementById('krishnaAudio');
+    this.init();
+  }
 
-        // document.addEventListener('mousemove',(e)=>{
-        //     this.mouseX=e.clientX;
-        //     this.mouseY=e.clientY;
+  init() {
+    this.paper.addEventListener('mousedown', (e) => {
+      this.holdingPaper = true;
+      this.paper.style.zIndex = highestZ;
+      highestZ += 1;
 
-        //     this.velocityX=this.mouseX-this.prevMouseX;
-        //     this.velocityY=this.mouseY-this.prevMouseY;
+      if (e.button === 0) {
+        this.prevMouseX = this.mouseX;
+        this.prevMouseY = this.mouseY;
+      }
 
-        //     if(this.holdingPaper){
-        //         this.currentPaperX += this.velocityX;
-        //         this.currentPaperY +=this.velocityY;
+      // Check if the paper is Krishna and play audio on single click
+      if (this.paper.classList.contains('Krishna') && e.detail === 1) {
+        this.audio.play();
+      }
+    });
 
-        //         this.prevMouseX=this.mouseX;
-        //         this.prevMouseY=this.mouseY;
+    this.paper.addEventListener('dblclick', (e) => {
+      // Check if the paper is Krishna and pause audio on double click
+      if (this.paper.classList.contains('Krishna')) {
+        this.audio.pause();
+      }
+    });
 
-        //         paper.style.transform=`translateX(${this.currentPaperX})px translateY(${this.currentPaperY})px`;
-        //     }
-        // })
-        document.addEventListener('mousemove', (e) => {
-            this.mouseX = e.clientX;
-            this.mouseY = e.clientY;
-        
-            if (this.holdingPaper) {
-                this.velocityX = this.mouseX - this.prevMouseX;
-                this.velocityY = this.mouseY - this.prevMouseY;
-        
-                this.currentPaperX += this.velocityX;
-                this.currentPaperY += this.velocityY;
-        
-                this.prevMouseX = this.mouseX;
-                this.prevMouseY = this.mouseY;
-        
-                paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px)`;
-            }
-        });
-        
+    document.addEventListener('mousemove', (e) => {
+      this.mouseX = e.clientX;
+      this.mouseY = e.clientY;
 
-        window.addEventListener('mouseup',(e)=>{
-            console.log('mouse release')
-            this.holdingPaper=false;
-        })
-    }
+      if (this.holdingPaper) {
+        this.velocityX = this.mouseX - this.prevMouseX;
+        this.velocityY = this.mouseY - this.prevMouseY;
+
+        this.currentPaperX += this.velocityX;
+        this.currentPaperY += this.velocityY;
+
+        this.prevMouseX = this.mouseX;
+        this.prevMouseY = this.mouseY;
+
+        this.paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px)`;
+      }
+    });
+
+    window.addEventListener('mouseup', () => {
+      this.holdingPaper = false;
+    });
+  }
 }
-const papers=Array.from(document.querySelectorAll('.paper'));
-papers.forEach((paper)=>{
-    const p=new Paper();
-    p.init(paper);
 
-})
+document.addEventListener('DOMContentLoaded', () => {
+  const papers = Array.from(document.querySelectorAll('.paper'));
+  papers.forEach((paper) => {
+    const p = new Paper(paper);
+  });
+});
+
